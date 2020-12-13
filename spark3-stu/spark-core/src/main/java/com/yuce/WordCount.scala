74d7df4fcf2cc0d1e7b53f6e2b46738f6ac83482
jetli <2835138969@qq.com>
2020-12-13 21:49:20 +0800

    test01

--- spark3-stu/spark-core/src/main/java/com/yuce/WordCount.scala
+++ spark3-stu/spark-core/src/main/java/com/yuce/WordCount.scala
@@ -9,13 +9,14 @@ import org.apache.spark.{SparkConf, SparkContext}
   */
 object WordCount {
   def main(args: Array[String]): Unit = {
-    val conf: SparkConf = new SparkConf().setAppName("WordCount").setMaster("local[*]")
+    val conf: SparkConf = new SparkConf().setAppName("WordCount").setMaster("local[1]")
     val sc = new SparkContext(conf)
     val value: RDD[String] = sc.textFile("C:\\companyFile\\spark3-stu\\datas\\word")
     val word1: RDD[String] = value.flatMap(_.split(" "))
     val word2: RDD[(String, Int)] = word1.map((_,1))
     val word3: RDD[(String, Int)] = word2.reduceByKey(_ + _)
     val tuples: Array[(String, Int)] = word3.collect()
+    //test
     tuples.foreach(println)
     sc.stop()
   }
